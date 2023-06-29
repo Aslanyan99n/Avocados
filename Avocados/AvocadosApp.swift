323c460110e1dbbcfed71e944e19a24d77d31338
Aslanyan99n <a99narek@gmail.com>
2023-06-29 09:25:00 +0400

    Initial Commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Narek on 29.06.23.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
